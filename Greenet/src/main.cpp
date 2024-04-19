6ef90db986209d04474cc62b86eda2c09fda9f17
WiraGd <155901717+WiraGd@users.noreply.github.com>
2024-04-19 11:35:59 +0800

    Update main.cpp

--- Greenet/src/main.cpp
+++ Greenet/src/main.cpp
@@ -3,7 +3,10 @@ const char* ssid = "Wokwi-GUEST"; //masukan nama wifi
 const char* password = ""; //masukan password wifi
 
 void setup() {
-
+  Serial.begin(115200);
+  WiFi.mode(WIFI_STA);
+  Serial.print("Connecting to WiFi network: ");
+  Serial.println(ssid);
 }
 
 void loop() {
