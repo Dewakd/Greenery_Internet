fc659dcad25f830781c691e5ba6114cd469b7209
dhipapradnya <162403046+dhipapradnya@users.noreply.github.com>
2024-04-19 17:38:14 +0800

    menambah setup wifi

--- Microclimate-Adjuster/src/main.cpp
+++ Microclimate-Adjuster/src/main.cpp
@@ -68,7 +68,12 @@ void reconnect() {
     }
   }
 }
-
+void setup() {
+  Serial.begin(115200);
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+}
 
       
     }
