ef5b40350eff3594fbf0fbcf47ce86b51bc6275f
benartdo <wbenartdo@gmail.com>
2024-04-19 21:27:14 +0800

    add a main

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,15 @@
-#include <Arduino.h>
+#include <WiFi.h>
+#include <PubSubClient.h>
 
-// put function declarations here:
-int myFunction(int, int);
+const char* ssid = "UDAWA";
+const char* password = "defaultkey";
+const char* mqtt_server = "broker.hivemq.com"; // ubah address MQTT broker
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
-}
+WiFiClient espClient;
+PubSubClient client(espClient);
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
+void setup_wifi() {
+  delay(10);
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
