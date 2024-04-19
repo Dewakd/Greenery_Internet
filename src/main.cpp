f13d45247b9b723ad414671895930aabb8564200
Rioadityapratama <162407264+Rioadityapratama@users.noreply.github.com>
2024-04-19 21:47:41 +0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -30,3 +30,20 @@ void callback(char* topic, byte* payload, unsigned int length) {
   Serial.print("Message arrived [");
   Serial.print(topic);
   Serial.print("] ");
+
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+
+  String topics = String(topic);
+  if(topics == "undiknas/FTI/TI/2022/IoT/Aditya/lampu"){
+    if(payload[0] == 1 ){
+      Serial.println("Nyalakan Lampu...");
+    }
+    else{
+      Serial.println("Matikan Lampu...");
+    }
+  }
+
+}
