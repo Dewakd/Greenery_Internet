9dce5698fe21ea7bd47beac108a7e0e7d145c9a4
RizkyAnggika <162704097+RizkyAnggika@users.noreply.github.com>
2024-04-19 17:23:55 +0800

    menambahkan fungsi untuk callback

--- Microclimate-Adjuster/src/main.cpp
+++ Microclimate-Adjuster/src/main.cpp
@@ -27,3 +27,25 @@ void setup_wifi() {
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
 }
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+
+  String topics = String(topic);
+  if(topics == "undiknas/FTI/TI/2022/IoT/Aditya/lampu"){
+    if(payload[0] == 1 ){
+      Serial.println("Nyalakan Lampu...");
+    }
+    else{
+      Serial.println("Matikan Lampu...");
+    }
+  }
+
+}
