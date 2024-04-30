38d5f0beb6a9ab5df57e15c91b0a1be2b646f055
DanendraPutra <167526492+DanendraPutra@users.noreply.github.com>
2024-04-30 12:24:59 +0800

    satukan pasukan serangan pertama

--- FertigationPumpAgent/src/main.cpp
+++ FertigationPumpAgent/src/main.cpp
@@ -1,91 +1,54 @@
-#include <WiFi.h>
-#include <PubSubClient.h>
+#include <Arduino.h>
+#include <MQTT.h>
+#include <UrusanWiFi.h>
+#include <UrusanIoT.h>
+#include "secret.h"
+#include <TaskScheduler.h>
 
-const char* ssid = "UDAWA";
-const char* password = "defaultkey";
-const char* mqtt_server = "broker.hivemq.com"; // Change this to the address of your MQTT broker
+void penangkapPesan(String topic, String message);
+void task1DetailTugas();
 
-WiFiClient espClient;
-PubSubClient client(espClient);
+UrusanWiFi urusanWiFi(ssid, pass);
+UrusanIoT urusanIoT(broker, port, id, brokerUsername, brokerPassword);
+Scheduler penjadwal;
 
-void setup_wifi() {
-  delay(10);
-  // We start by connecting to a WiFi network
-  Serial.println();
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
-
-  WiFi.begin(ssid, password);
-
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-
-  Serial.println("");
-  Serial.println("WiFi connected");
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
-}
-
-void callback(char* topic, byte* payload, unsigned int length) {
-  Serial.print("Message arrived [");
-  Serial.print(topic);
-  Serial.print("] ");
-
-  for (int i = 0; i < length; i++) {
-    Serial.print((char)payload[i]);
-  }
-  Serial.println();
-
-  String topics = String(topic);
-  if(topics == "undiknas/FTI/TI/2022/IoT/Aditya/lampu"){
-    if(payload[0] == 1 ){
-      Serial.println("Nyalakan Lampu...");
-    }
-    else{
-      Serial.println("Matikan Lampu...");
-    }
-  }
-<<<<<<< HEAD:PlatformIO/Projects/Fergation_Pump_Agent/src/main.cpp
-
-=======
-}
-
-void reconnect() {
-  // Loop until we're reconnected
-  while (!client.connected()) {
-    Serial.print("Attempting MQTT connection...");
-    // Attempt to connect
-    if (client.connect("Aditya123555777")) {
-      Serial.println("connected");
-      // Once connected, subscribe to the topic you wish to receive messages on
-      client.subscribe("undiknas/FTI/TI/2022/IoT/Aditya/#");
-    } else {
-      Serial.print("failed, rc=");
-      Serial.print(client.state());
-      Serial.println(" try again in 5 seconds");
-      // Wait 5 seconds before retrying
-      delay(5000);
-    }
-  }
-}
-
+Task task1(3000, TASK_FOREVER, &task1DetailTugas);
 
 void setup() {
+  // put your setup code here, to run once:
   Serial.begin(115200);
-  setup_wifi();
-  client.setServer(mqtt_server, 1883);
-  client.setCallback(callback);
+
+  urusanWiFi.konek();
+  urusanIoT.konek();
+  urusanIoT.penangkapPesan(penangkapPesan);
+  urusanIoT.subscribe("id/greenet/fergationpumpagent");
+
+  penjadwal.init();
+  penjadwal.addTask(task1);
+  task1.enable();
 }
 
-
-
 void loop() {
-  if (!client.connected()) {
-    reconnect();
+  // put your main code here, to run repeatedly:
+  urusanIoT.proses();
+
+  if(urusanWiFi.apakahKonek() == 1 && urusanIoT.apakahKonek() == 0){
+    urusanIoT.konek();
   }
-  client.loop();
-  // Other loop code goes here
->>>>>>> b6f9e908def811c813aee11a259377457fe47228:src/main.cpp
+
+  penjadwal.execute();
+}
+
+/// @brief Fungsi callback dari fungsi subscribe objek urusanIoT
+/// @param topic 
+/// @param message 
+void penangkapPesan(String topic, String message){
+  Serial.printf("penangkapPesan: topic: %s | message: %s\n", topic.c_str(), message.c_str());
+}
+
+/// @brief Fungsi callback dari task1
+void task1DetailTugas(){
+  if(urusanIoT.apakahKonek() == 1){
+    urusanIoT.publish("id/greenet/fergationpumpagent");
+  }
 }
