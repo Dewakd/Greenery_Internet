9c85f468d97f87f40c1c28786cdc7a40ed36759a
MZfran287 <136235701+MZfran287@users.noreply.github.com>
2024-04-30 11:15:19 +0800

    Project Directory Refactor

--- FertigationReservoirAgent/Fertigation Reservoir Agent/src/secret.h
+++
@@ -1,5 +0,0 @@
-const char* ssid = "MyWiFi";
-const char* pass = "bukanbudakai";
-const char* id = "com.elangnesia.microclimateconditionagent";
-const char* broker = "broker.hivemq.com";
-const int port = 1883;
